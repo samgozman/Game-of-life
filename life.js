4a1ecd1bc1eab774ef8497d437f82504f436eaee
Sam Gozman <eragonovich@gmail.com>
2016-10-08 20:18:56 +0300

    Разбил на два холста, функция рисования точек

--- life.js
+++ life.js
@@ -1,28 +1,31 @@
 'use strict'; //ES5 строгий режим
-var CELL_SIZE = 8; //размер клетки
+var console;
+
+var CELL_SIZE = 32; //размер клетки
 var cells = [ [], [] ]; //многомерный костыль JS
-var canvas;
+var canvas, game;
 
 function init() {
-    canvas = document.getElementById('game').getContext('2d');
-    canvas.width =  document.getElementById('game').width;
-    canvas.height =  document.getElementById('game').height;
-
+    //back-grid
+    canvas = document.getElementById('back').getContext('2d');
+    canvas.width =  document.getElementById('back').width;
+    canvas.height =  document.getElementById('back').height;
+    
+    //game
+    game = document.getElementById('game').getContext('2d');
+    
     //grid
     function Grid() {
         this.size = { x : 0, y : 0 };
         this.width = canvas.width;
         this.height = canvas.height;
-        
-        //canvas.fillRect(0, 0, canvas.width, canvas.height);
-        
 
         //grid functions
         
-        this.size.x = Math.floor(512 / CELL_SIZE); //округляем к наименьшему (оптимизировать)
-        this.size.y = Math.floor(512 / CELL_SIZE);
+        this.size.x = Math.floor(canvas.width / CELL_SIZE); //округляем к наименьшему (оптимизировать)
+        this.size.y = Math.floor(canvas.height / CELL_SIZE);
         
-
+        //заполняем массив cells
         this.fill = function () {
             //cell верхяя левая и нижняя левая границы
             var i, j;
@@ -32,10 +35,13 @@ function init() {
                 }
             }
         };
-
+        
+        //рисуем сетку
         this.draw = function () {
             var i;
-            window.alert(this.size.x);
+            
+            console.log(this.size.x);
+            
             canvas.translate(0.5, 0.5);
             canvas.beginPath();
             for (i = 0; i <= this.size.y; i += 1) {
@@ -50,10 +56,32 @@ function init() {
             }
             canvas.stroke();
         };
-
     }
-    var kek = new Grid();
-    kek.draw();
-}
+        
+    //обновляем отрисовку
+    function Update() {
+        this.clear = function () {
+            game.clearRect(0, 0, canvas.width, canvas.height);
+        };
 
+        this.fillCell = function (x, y) {
+            game.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+        };
+    }
+
+    var gameGrid = new Grid();
+    var gameUpd = new Update();
+    gameGrid.draw();
+   // gameGrid.fill();
+    gameUpd.fillCell(10, 10);
+    gameUpd.fillCell(11, 10);
+    gameUpd.fillCell(12, 10);
+    //gameUpd.clear();
+    var clearBtn = document.getElementById("clear");
+    clearBtn.onclick = function () { gameUpd.clear(); };
+
+
+}
+    
+    
 window.onload = init();

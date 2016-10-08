58e6dd3e26ad410b86c1fe609e4522a5ae2f3639
Sam Gozman <eragonovich@gmail.com>
2016-10-08 13:13:59 +0300

    Fill cells

--- life.js
+++ life.js
@@ -1,7 +1,7 @@
 'use strict'; //ES5 строгий режим
 var canvas = document.getElementById('game').getContext('2d');
 var CELL_SIZE = 8; //размер клетки
-var cells = [ [], [] ]; //многомерный костыль JS
+var cells:Boolean = [ [], [] ]; //многомерный костыль JS
 
 //grid
 function Grid() {
@@ -10,11 +10,21 @@ function Grid() {
     
     //grid functions
     this.size = function (width, height) {
-        this.x = Math.floor(width / CELL_SIZE); //округляем к наименьшему (оптимизировать)
-        this.y = Math.floor(height / CELL_SIZE);
+        this.size.x = Math.floor(width / CELL_SIZE); //округляем к наименьшему (оптимизировать)
+        this.size.y = Math.floor(height / CELL_SIZE);
     };
     
     this.fill = function () {
-        
+        //cell верхяя левая и нижняя левая границы
+        var i, j;
+        for (i = 0; i < this.width; i += CELL_SIZE) {
+            for (j = 0; j < this.height; j += CELL_SIZE) {
+                cells[i, j] = false; //false - нет жизни, true есть
+            }
+        }
+    };
+    
+    this.draw = function () {
+         
     };
 }
